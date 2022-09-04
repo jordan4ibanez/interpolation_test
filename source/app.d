ba346f446b1354adca1dab20e0920199a13ef697
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-04 00:22:39 -0400

    Implement more information/corrections

--- source/app.d
+++ source/app.d
@@ -11,46 +11,64 @@ void main() {
     double destination = 0;
     double progress = 0;
 
+    // This multiplier can be controlled based on how fast you want the object to interpolate
+    double speedMultiplier = 3.0;
+
     bool doNew = true;
+
+    // These variable are soley used for testing!
+    double diff = 0.0;
+    double accumulator = 0.0;
 
     // This should be an entity switch if it gets a new position
     while(tests < 20) {
-        assert(Math.random() != 1);
         calculateDelta();
 
         if (doNew) {
+            writeln("--------------------------------------------");
+            writeln("TEST ", tests);
+            writeln("--------------------------------------------");
+
+
             progress = 0.0;
             origin = yaw;
             destination = (Math.random() * 360.0) - 180.0;
             doNew = false;
+            accumulator = 0.0;
 
-            if (Math.abs(origin) + Math.abs(destination) >= 180.0) {
-                writeln("overshooting");
+            diff = Math.abs(origin) + Math.abs(destination);
+
+
+            // writeln("the test: ", internalSpeedMultiplier);
+
+            if (diff >= 180.0) {
+                // writeln("overshooting");
                 // Overshoot it and correct later
                 // The signum is a multiplier based on if it's negative or positive (is 1 or -1)
-                writeln("original destination: ", destination);
+                // writeln("original destination: ", destination);
                 double signum = Math.signum(destination);
                 destination = (Math.abs(destination) + 180.0) * signum;
-                writeln("corrected destination: ", destination);
+                // writeln("corrected destination: ", destination);
             } else {
-                writeln("not overshooting");
+                // writeln("not overshooting");
             }
         }
+
+        accumulator += getDelta();
 
         // In production, STOP doing the interpolation
         // Perhaps give the entity object a bool that is "interpolateNow"?
         if (progress >= 1.0) {
             progress = 1.0;
             doNew = true;
+            tests++;
         } else {
             // Goal has not been reached
             // This is the progress of the linear interpolation
-            progress += getDelta();
+            progress += getDelta() * speedMultiplier;
         }
 
         yaw = Math.lerp(origin, destination, progress);
-
-        // writeln(yaw);
         
         // This is the overshoot correction
         yawCorrection(yaw);
@@ -59,10 +77,13 @@ void main() {
             writeln("--------------------------------------------");
             writeln("Destination reached! Destination was: ", destination);
             writeln("The real yaw is now: ", yaw);
+            writeln("The diff was: ", diff);
+            writeln("It took roughtly: ", accumulator, " seconds");
             writeln("--------------------------------------------");
         }
 
-        writeln("yaw is: ", yaw);
+        // Uncomment this to see live info on yaw
+        // writeln("yaw is: ", yaw);
 
         assert(yaw >= -180.0 && yaw <= 180.0, "you done goofed");
 
