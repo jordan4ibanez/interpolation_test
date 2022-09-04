e31b182d08b3609a0171ff4dd974e163d208df9a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-03 23:13:05 -0400

    Add basic constraints

--- source/app.d
+++ source/app.d
@@ -1,6 +1,36 @@
 import std.stdio;
+import Math = math;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+void main() {
+    int tests = 0;
+    double yaw = 0;
+    double goal = 0;
+    bool doNew = true;
+
+    while(tests < 20) {
+
+        if (doNew) {
+            goal = (Math.random() * 360.0) - 180.0;
+            doNew = false;
+        }
+
+        yaw = Math.lerp(yaw, goal, 1.0);
+
+        yawCorrection(yaw);
+
+        writeln("yaw is: ", yaw);
+
+        assert(yaw >= -180.0 && yaw <= 180.0, "you done goofed");
+    }
+	
+}
+
+void yawCorrection(ref double yaw) {
+    if (yaw < -180.0) {
+        yaw += 360.0;
+        writeln("yaw has underflowed ---------");
+    } else if (yaw > 180.0) {
+        yaw -= 360.0;
+        writeln("yaw has overflowed +++++++++");
+    }
 }
